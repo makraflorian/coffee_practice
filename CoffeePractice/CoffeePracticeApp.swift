b694f3d23f90b27025a88774fd9d02025172f7e8
Makra Flórián <makraflorian10@gmail.com>
2024-02-01 10:58:22 +0100

    initial commit

---
+++ CoffeePractice/CoffeePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeePracticeApp.swift
+//  CoffeePractice
+//
+//  Created by Makra Flórián Róbert on 2024. 02. 01..
+//
+
+import SwiftUI
+
+@main
+struct CoffeePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
